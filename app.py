7f6462156f1e3936e98ae1c1db1f9c35415f23c5
zongOuO <wf10030119@gmail.com>
2024-08-07 22:29:45 +0800

    增加對話記憶(測試)

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
 from groq import Groq
+from firebase import firebase
+
 
 app = Flask(__name__)
 
 # Initialize LINE Bot API and Webhook Handler
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+firebase_url = os.getenv('FIREBASE_URL')
 
 # Initialize Groq Client
 groq_client = Groq(api_key=os.environ['GROQ_API_KEY'])
@@ -31,25 +34,32 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    fdb = firebase.FirebaseApplication(firebase_url, None)
+    user_id = event['source']['userId']
+    user_chat_path = f'chat/{user_id}'
     user_message = event.message.text
     try:
-        response = groq_client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": "你只會繁體中文，回答任何問題時，都會使用繁體中文回答"
-                },
-                {
-                    "role": "user",
-                    "content": user_message,
-                }
-            ],
-            model="llama3-8b-8192",
-        )
-        app.logger.info(f"Groq API response: {response}")
-
-        # Assuming the response contains a 'choices' list with 'message' field
-        response_text = response.choices[0].message.content
+        if user_message == "!清空":
+            response_text = "對話歷史紀錄已經清空！"
+            fdb.delete(user_chat_path, None)
+        else:
+            response = groq_client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "你只會繁體中文，回答任何問題時，都會使用繁體中文回答"
+                    },
+                    {
+                        "role": "user",
+                        "content": user_message,
+                    }
+                ],
+                model="llama3-8b-8192",
+            )
+            app.logger.info(f"Groq API response: {response}")
+            # Assuming the response contains a 'choices' list with 'message' field
+            response_text = response.choices[0].message.content
+            fdb.put_async(user_chat_path, None , response_text)
     except Exception as e:
         app.logger.error(f"Groq API error: {e}")
         response_text = "抱歉，目前無法處理您的請求。"
